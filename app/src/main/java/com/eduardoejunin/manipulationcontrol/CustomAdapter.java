54f1aa848cfca3dd072e1c55068a6ef4dff3c3df
igorvsmacedo <igorvsmacedo@gmail.com>
2023-11-18 18:49:33 -0300

    Adiçao do Custom adapter pra personalizar a listView ListView com botões Obs. cloquei pra chamar a dashboard na main activity, porém não estou subindo a alteração no git; metodo add na lista e remover com confrirmação de dialog, todos os métodos estão comentados :)

---
+++ app/src/main/java/com/eduardoejunin/manipulationcontrol/CustomAdapter.java
@@ -0,0 +1,110 @@
+package com.eduardoejunin.manipulationcontrol;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class CustomAdapter extends BaseAdapter {
+
+    private Context context;
+    private List<String> dataList;
+
+    public CustomAdapter(Context context, List<String> dataList) {
+        this.context = context;
+        this.dataList = dataList;
+    }
+
+    @Override
+    public int getCount() {
+        return dataList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return dataList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+
+    @Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        View view = LayoutInflater.from(context).inflate(R.layout.list_item, null);
+
+        TextView textView = view.findViewById(R.id.textView);
+        Button btnAlterar = view.findViewById(R.id.btnAlterar);
+        Button btnExcluir = view.findViewById(R.id.btnExcluir);
+        Button btnAprovar = view.findViewById(R.id.btnAprovar);
+
+        // Configurar o texto e o clique do botão
+        textView.setText(dataList.get(position));
+        btnAlterar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Ação do botão
+                Toast.makeText(context, "Alterar", Toast.LENGTH_SHORT).show();
+            }
+        });
+        btnExcluir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Ação do botão exluir
+                showConfirmationDialog(position);
+          //      dataList.remove(position );
+             //   notifyDataSetChanged();
+                Toast.makeText(context, "Excluir", Toast.LENGTH_SHORT).show();
+            }
+        });
+        btnAprovar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Ação do botão aprovar
+                Toast.makeText(context, "Aprovar", Toast.LENGTH_SHORT).show();
+            }
+        });
+        return view;
+    }
+    private void showConfirmationDialog(final int position) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("ATENÇÃO");
+        builder.setMessage("Tem certeza de que deseja excluir este item?");
+
+        // Adiciona botões "Sim" e "Não"
+        builder.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Lógica para excluir o item
+                dataList.remove(position);
+                notifyDataSetChanged();
+                Toast.makeText(context, "Item excluído com sucesso", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        builder.setNegativeButton("Não", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // Nada a fazer se o usuário clicar em "Não"
+            }
+        });
+
+        // Mostra o diálogo
+        builder.create().show();
+    }
+
+}
--- app/src/main/java/com/eduardoejunin/manipulationcontrol/Dashboard.java
+++ app/src/main/java/com/eduardoejunin/manipulationcontrol/Dashboard.java
@@ -1,8 +1,18 @@
 package com.eduardoejunin.manipulationcontrol;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Dashboard extends AppCompatActivity {
 
@@ -10,5 +20,26 @@ public class Dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        ListView listView = findViewById(R.id.listViewPedidos);
+        List<String> dataList = new ArrayList<>();
+        CustomAdapter adapter = new CustomAdapter(this, dataList);
+        listView.setAdapter(adapter);
+        dataList.add("Pedido 1");
+        dataList.add("Pedido 2");
+        dataList.add("Pedido 3");
+        Button btnAdicionarPedido = findViewById(R.id.btnAdicionarPedido);
+        btnAdicionarPedido.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int count = adapter.getCount() + 1;
+                dataList.add("Pedido " + count);
+                Toast.makeText(getApplicationContext(), "Adicionado", Toast.LENGTH_SHORT).show();
+                adapter.notifyDataSetChanged();
+
+            }
+        });
+
+
     }
 }
