d896d8a90030ba3336661519bc8cd0ebd1df0546
Eduardo <eduardo.munhoz97@outlook.com>
2023-11-18 18:07:38 -0300

    correção de commit

--- app/src/main/java/com/eduardoejunin/manipulationcontrol/CadastroUsuario.java
+++ app/src/main/java/com/eduardoejunin/manipulationcontrol/CadastroUsuario.java
@@ -3,6 +3,8 @@ package com.eduardoejunin.manipulationcontrol;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CadastroUsuario extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class CadastroUsuario extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cadastro_usuario);
+
+        Button btVoltar = findViewById(R.id.btVoltar);
+
+        btVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
